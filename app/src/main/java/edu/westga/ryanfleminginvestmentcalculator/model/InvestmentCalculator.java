7395ee0233005fcf9c3646a6a4a6ae2e3c36dfa9
Ryan Fleming <ryantfleming@aol.com>
2016-03-04 17:37:43 -0500

    Created getValue() method in the InvestmentCalculator, and also created a test that returns 0.0 when all parameters are 0.0 and when only payment is 0.0

--- app/src/main/java/edu/westga/ryanfleminginvestmentcalculator/model/InvestmentCalculator.java
+++ app/src/main/java/edu/westga/ryanfleminginvestmentcalculator/model/InvestmentCalculator.java
@@ -8,4 +8,24 @@ package edu.westga.ryanfleminginvestmentcalculator.model;
  */
 public class InvestmentCalculator {
 
+    final private double paymentAmount;
+    final private double theRate;
+    final private int paymentPeriod;
+
+    /**
+     * Constructor for the Investement calculator. Accepts a payment amount,
+     * a rate, and a number of periods for the calculation.
+     * @param payment - the amount of each payment
+     * @param rate - the percentage rate for each period
+     * @param period - the number of periods the payment will be made
+     */
+    public InvestmentCalculator(double payment, double rate, int period) {
+        this.paymentAmount = payment;
+        this.theRate = rate;
+        this.paymentPeriod = period;
     }
+
+    public double getValue() {
+        return 0.0;
+    }
+}
--- app/src/test/java/edu/westga/ryanfleminginvestmentcalculator/CalculateFutureValueTest.java
+++ app/src/test/java/edu/westga/ryanfleminginvestmentcalculator/CalculateFutureValueTest.java
@@ -2,6 +2,8 @@ package edu.westga.ryanfleminginvestmentcalculator;
 
 import org.junit.Test;
 
+import edu.westga.ryanfleminginvestmentcalculator.model.InvestmentCalculator;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,8 +11,19 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class CalculateFutureValueTest {
+    InvestmentCalculator testCalc;
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void shouldReturn0WhenPaymentAndRateAndPeriodAre0() throws Exception {
+        this.testCalc = new InvestmentCalculator(0.0, 0.0, 0);
+        assertEquals(0.0, testCalc.getValue(), 2);
     }
+
+    @Test
+    public void shouldReturn0WhenOnlyPaymentIs0() throws Exception {
+        this.testCalc = new InvestmentCalculator(0.0, 10.0, 5);
+        assertEquals(0.0, testCalc.getValue(), 2);
+    }
+
+
 }
