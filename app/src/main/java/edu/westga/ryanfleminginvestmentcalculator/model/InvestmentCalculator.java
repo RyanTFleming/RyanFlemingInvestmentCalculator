554a17120e69f91929e8cc80ac4a45c932df8b4d
Ryan Fleming <ryantfleming@aol.com>
2016-03-04 12:43:32 -0500

    Created InvestmentCalculator class and it's related JUnit test class

---
+++ app/src/main/java/edu/westga/ryanfleminginvestmentcalculator/model/InvestmentCalculator.java
@@ -0,0 +1,11 @@
+package edu.westga.ryanfleminginvestmentcalculator.model;
+
+/**
+ * Model for the Investment calculator. Calculates the future value of an
+ * annuity
+ *
+ * Created by Ryan on 3/4/2016.
+ */
+public class InvestmentCalculator {
+
+    }
--- app/src/test/java/edu/westga/ryanfleminginvestmentcalculator/CalculateFutureValueTest.java
+++ app/src/test/java/edu/westga/ryanfleminginvestmentcalculator/CalculateFutureValueTest.java
@@ -5,9 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
+ * Test case for the InvestmentCalculator Classe.
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+public class CalculateFutureValueTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
