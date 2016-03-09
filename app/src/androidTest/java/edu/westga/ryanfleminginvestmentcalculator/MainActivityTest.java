cb9a16be4ef51d699dea07bd9ecc1d47195c49d2
Ryan Fleming <ryantfleming@aol.com>
2016-03-09 14:57:32 -0500

    Added MainActivityTest method to test that the value in the results textview is correct and correctly formatted when the proper inputs are calculated. Also fixed the formatting of the results. Also created tests to insure the proper error messages are displayed and added the code to make them display

--- app/src/androidTest/java/edu/westga/ryanfleminginvestmentcalculator/MainActivityTest.java
+++ app/src/androidTest/java/edu/westga/ryanfleminginvestmentcalculator/MainActivityTest.java
@@ -1,6 +1,10 @@
 package edu.westga.ryanfleminginvestmentcalculator;
 
+import android.app.Activity;
+import android.os.SystemClock;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,27 +28,164 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         MainActivity activity = getActivity();
         TextView resultTextView = (TextView) activity.findViewById(R.id.textViewResult);
         String result = resultTextView.getText().toString();
-        assertEquals(result, "0.00");
+        assertEquals("$0.00", result);
     }
 
     public void testInitialPaymentValue0() {
         MainActivity activity = getActivity();
         EditText paymentTextView = (EditText) activity.findViewById(R.id.editTextPayment);
         String payment = paymentTextView.getText().toString();
-        assertEquals(payment, "0.00");
+        assertEquals("0.00", payment);
     }
 
     public void testInitialRateValue0() {
         MainActivity activity = getActivity();
         EditText rateTextView = (EditText) activity.findViewById(R.id.editTextRate);
         String rate = rateTextView.getText().toString();
-        assertEquals(rate, "0.00");
+        assertEquals("0.00", rate);
     }
 
     public void testInitialPeriodValue0() {
         MainActivity activity = getActivity();
         EditText periodTextView = (EditText) activity.findViewById(R.id.editTextPeriod);
         String period = periodTextView.getText().toString();
-        assertEquals(period, "0");
+        assertEquals("0", period);
+    }
+
+    public void testResultValueShouldBeSetWhenValidValuesAndButtonClick() throws Exception {
+        final MainActivity activity = getActivity();
+        final EditText paymentEdit = (EditText) activity.findViewById(R.id.editTextPayment);
+        final EditText rateEditText = (EditText) activity.findViewById(R.id.editTextRate);
+        final EditText periodEditText = (EditText) activity.findViewById(R.id.editTextPeriod);
+
+        this.getFocus(paymentEdit);
+        this.clearEditText();
+        getInstrumentation().sendStringSync("100.0");
+
+        this.getFocus(rateEditText);
+        this.clearEditText();
+        getInstrumentation().sendStringSync("10.0");
+
+        this.getFocus(periodEditText);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DPAD_RIGHT);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DEL);
+        getInstrumentation().sendStringSync("10");
+
+        this.clickButton(activity);
+
+        assertEquals("$1,593.74", getResultString(activity));
+
+    }
+
+    public void testResultsWithInvalidPayment() {
+        final MainActivity activity = getActivity();
+        final EditText paymentEdit = (EditText) activity.findViewById(R.id.editTextPayment);
+        this.getFocus(paymentEdit);
+        this.clearEditText();
+        this.clickButton(activity);
+        assertEquals("Invalid Payment", getResultString(activity));
+    }
+
+    public void testResultsWithInvalidRate() {
+        final MainActivity activity = getActivity();
+        final EditText rateEdit = (EditText) activity.findViewById(R.id.editTextRate);
+        this.getFocus(rateEdit);
+        this.clearEditText();
+        this.clickButton(activity);
+        assertEquals("Invalid Rate", getResultString(activity));
+    }
+
+    public void testResultsWithInvalidPeriod() {
+        final MainActivity activity = getActivity();
+        final EditText periodEdit = (EditText) activity.findViewById(R.id.editTextPeriod);
+        this.getFocus(periodEdit);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DPAD_RIGHT);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DEL);
+        this.clickButton(activity);
+        assertEquals("Invalid Period", getResultString(activity));
+    }
+
+    public void testResultsWithInvalidPaymentAndRate() {
+        final MainActivity activity = getActivity();
+        final EditText paymentEdit = (EditText) activity.findViewById(R.id.editTextPayment);
+        final EditText rateEdit = (EditText) activity.findViewById(R.id.editTextRate);
+        this.getFocus(paymentEdit);
+        this.clearEditText();
+        this.getFocus(rateEdit);
+        this.clearEditText();
+        this.clickButton(activity);
+        assertEquals("Invalid Payment\nInvalid Rate", getResultString(activity));
+    }
+
+    public void testResultsWithInvalidPaymentAndPeriod() {
+        final MainActivity activity = getActivity();
+        final EditText paymentEdit = (EditText) activity.findViewById(R.id.editTextPayment);
+        final EditText periodEdit = (EditText) activity.findViewById(R.id.editTextPeriod);
+        this.getFocus(paymentEdit);
+        this.clearEditText();
+        this.getFocus(periodEdit);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DPAD_RIGHT);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DEL);
+        this.clickButton(activity);
+        assertEquals("Invalid Payment\nInvalid Period", getResultString(activity));
+    }
+
+    public void testResultsWithInvalidRateAndPeriod() {
+        final MainActivity activity = getActivity();
+        final EditText rateEdit = (EditText) activity.findViewById(R.id.editTextRate);
+        final EditText periodEdit = (EditText) activity.findViewById(R.id.editTextPeriod);
+        this.getFocus(rateEdit);
+        this.clearEditText();
+        this.getFocus(periodEdit);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DPAD_RIGHT);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DEL);
+        this.clickButton(activity);
+        assertEquals("Invalid Rate\nInvalid Period", getResultString(activity));
+    }
+
+    public void testResultsWithAllInvalidInputs() {
+        final MainActivity activity = getActivity();
+        final EditText paymentEdit = (EditText) activity.findViewById(R.id.editTextPayment);
+        final EditText rateEdit = (EditText) activity.findViewById(R.id.editTextRate);
+        final EditText periodEdit = (EditText) activity.findViewById(R.id.editTextPeriod);
+        this.getFocus(paymentEdit);
+        this.clearEditText();
+        this.getFocus(rateEdit);
+        this.clearEditText();
+        this.getFocus(periodEdit);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DPAD_RIGHT);
+        getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DEL);
+        this.clickButton(activity);
+        assertEquals("Invalid Payment\nInvalid Rate\nInvalid Period", getResultString(activity));
+    }
+
+    /* *********************** Private Methods ******************** */
+    private void clearEditText() {
+        for (int x = 0; x < 4; x++) {
+            getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DPAD_RIGHT);
+            getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DEL);
+        }
+    }
+
+    private void getFocus(EditText editText) {
+        final EditText focusedEditText = editText;
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                focusedEditText.requestFocus();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        SystemClock.sleep(500);
+    }
+
+    private String getResultString(Activity activity) {
+        TextView resultTextView = (TextView) activity.findViewById(R.id.textViewResult);
+        return resultTextView.getText().toString();
+    }
+
+    private void clickButton(Activity activity) {
+        Button button = (Button) activity.findViewById(R.id.btnCalculate);
+        TouchUtils.clickView(this, button);
     }
 }
--- app/src/main/java/edu/westga/ryanfleminginvestmentcalculator/MainActivity.java
+++ app/src/main/java/edu/westga/ryanfleminginvestmentcalculator/MainActivity.java
@@ -8,8 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.Format;
+
+import edu.westga.ryanfleminginvestmentcalculator.model.InvestmentCalculator;
 
 public class MainActivity extends AppCompatActivity {
+    private InvestmentCalculator calculator;
+    private TextView results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        this.results = (TextView) findViewById(R.id.textViewResult);
     }
 
     @Override
@@ -49,4 +59,79 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onCalculateBtnClick(View view) {
+        Double payment = this.getPaymentValue();
+        Double rate = this.getRateValue();
+        Integer period = this.getPeriodValue();
+        if (payment == null && rate == null && period == null) {
+            this.results.setText("Invalid Payment\nInvalid Rate\n" +
+                "Invalid Period");
+            return;
+        } else if (payment == null && rate == null) {
+            this.results.setText("Invalid Payment\nInvalid Rate");
+            return;
+        } else if (payment == null && period == null) {
+            this.results.setText("Invalid Payment\nInvalid Period");
+            return;
+        } else if (period == null && rate == null) {
+            this.results.setText("Invalid Rate\nInvalid Period");
+            return;
+        } else if (payment == null) {
+            this.results.setText("Invalid Payment");
+            return;
+        } else if (rate == null) {
+            this.results.setText("Invalid Rate");
+            return;
+        } else if (period == null) {
+            this.results.setText("Invalid Period");
+            return;
+        }
+
+        this.calculator = new InvestmentCalculator(payment, rate, period);
+        Double value = this.calculator.calculateValue();
+
+        String resultString = String.format("$%,.2f", value);
+
+        this.results.setText(resultString);
+
+
+
+
+    }
+
+    private Double getPaymentValue() {
+        EditText paymentTextBox = (EditText) this.findViewById(R.id.editTextPayment);
+        double payment;
+        try {
+            payment = Double.parseDouble(paymentTextBox.getText().toString());
+        } catch (NumberFormatException nfe) {
+            return null;
+        }
+        return payment;
+    }
+
+    private Double getRateValue() {
+        EditText rateTextBox = (EditText) this.findViewById(R.id.editTextRate);
+        double rate;
+        try {
+            rate = Double.parseDouble(rateTextBox.getText().toString());
+        } catch (NumberFormatException nfe) {
+            return null;
+        }
+        return rate;
+    }
+
+    private Integer getPeriodValue() {
+        EditText periodTextBox = (EditText) this.findViewById(R.id.editTextPeriod);
+        int period;
+        try {
+            period = Integer.parseInt(periodTextBox.getText().toString());
+        } catch (NumberFormatException nfe) {
+            return null;
+        }
+        return period;
+    }
+
+
 }
